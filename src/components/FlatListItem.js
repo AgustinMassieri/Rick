8431bdfa68d2a530b7ef0957f33efa1a1463076d
AgustinMassieri <agmassieri00@gmail.com>
2022-10-29 20:33:48 -0300

    Navigator and Firebase implementation

--- src/components/FlatListItem.js
+++ src/components/FlatListItem.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Text, Image, TouchableOpacity} from 'react-native';
+import {Text, Image, TouchableOpacity, SafeAreaView} from 'react-native';
 import styles from '../screens/MainStyles.js';
+
 
 const FlatListItem = ({item, setCharacterCurrent, setShowModal}) => {
 
     return (
-        <TouchableOpacity onPress={() => {
-            setCharacterCurrent(item);
-            setShowModal(true);
-            }}>
-            <Text style={styles.texto}>{item.name}</Text>
-            <Image style={styles.image} source={{uri: item.image}} />
-        </TouchableOpacity>
+        <SafeAreaView>
+            <TouchableOpacity onPress={() => {
+                setCharacterCurrent(item);
+                setShowModal(true);
+                }}>
+                <Text style={styles.texto}>{item.name}</Text>
+                <Image style={styles.image} source={{uri: item.image}} />
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
 
